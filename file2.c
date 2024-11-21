3f339ab74bdefbc3dd777544767bfbaba4365f78
dhanalakshmiviven <dhanalakshmithavva@gmail.com>
2024-11-21 14:43:59 +0530

    2ndfile

---
+++ file2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("enter i value\n");
+	scanf("%d",&i);
+	switch i
+	{
+		case 1:
+			printf("1");
+			break;
+		case 3:
+			printf("3");
+			break;
+		default:
+			exit;
+	}
+}
+
+
+
